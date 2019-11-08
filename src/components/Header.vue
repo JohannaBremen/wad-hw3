88ed77a987b82bf468608bd311898e474a6b7fb2
Johanna Angela Bremen <johanna.bremen@gmail.com>
2019-11-08 19:38:52 +0200

    task 1 & 2

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+            <strong>Welcome to your dashboard!</strong>
+    </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,47 @@
+<template>
+    <section id="container">
+            <section id="main">
+                <div class="content">
+                   <Profile :class="this.profileTabActive ? 'tab-active' : 'tab'"/>
+                   <Courses :class="this.coursesTabActive ? 'tab-active' : 'tab'"/>
+                </div>
+                <div class="controls">
+                    <button id="profile-button" :class="this.profileTabActive ? 'pill active' : 'pill'" @click="toggleProfile">Profile</button>
+                    <button id="courses-button" :class="this.coursesTabActive ? 'pill active' : 'pill'" @click="toggleCourses">Courses</button>
+                </div>
+            </section>
+        </section>
+</template>
+
+<script>
+import Profile from "./Profile.vue";
+import Courses from "./Courses.vue";
+
+export default {
+  name: "Main",
+  components: {
+    Profile,
+    Courses
+  },
+ data: () => {
+    return {
+      profileTabActive: true,
+      coursesTabActive: false
+    };
+  },
+  methods: {
+    toggleProfile: function() {
+      this.profileTabActive = true;
+      this.coursesTabActive = false;
+    },
+    toggleCourses: function() {
+      this.profileTabActive = false;
+      this.coursesTabActive = true;
+    }
+  }
+  
+};
+</script>
+
+<style scoped>
+</style>
